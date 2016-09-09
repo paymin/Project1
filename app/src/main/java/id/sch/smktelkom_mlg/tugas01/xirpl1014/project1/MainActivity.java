2e2d06c4767fade0597acfcac604a1eb9fb72282
Irsyad Rahman <irsyadrahman78@gmail.com>
2016-09-09 19:25:54 +0700

    Tambah Edit Text Nama + Email

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1014/project1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1014/project1/MainActivity.java
@@ -2,12 +2,40 @@ package id.sch.smktelkom_mlg.tugas01.xirpl1014.project1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    EditText etEmail;
+    Button bOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doProcess();
+            }
+        });
+    }
+
+    private void doProcess() {
+        String nama = etNama.getText().toString();
+        String email = etEmail.getText().toString();
+        tvHasil.setText("Hallo " + nama + " (" + email + ")");
     }
 }
